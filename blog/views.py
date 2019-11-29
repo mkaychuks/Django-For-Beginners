a8ec42914ee20d72fa971ce03c1a3841b5e5f2b8
Mkaychuks <sachufusi@gmail.com>
2019-11-29 17:00:39 +0100

    Added a DetalView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -7,8 +7,27 @@ from .models import Post
 class BlogListView(ListView):
     template_name = 'home.html'
     context_object_name = 'posts'
-    #model = Post
-    
-    def get_queryset(self):
-        post = Post.objects.order_by('-title')
-        return post
+    model = Post
+
+# Recreating the ListView using Function Based View   
+def blogListView(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    template_name = 'home.html'
+    return render(request, template_name, context)
+
+
+
+class BlogDetailView(DetailView):
+    template_name = 'post_detail.html'
+    model = Post
+
+def blogDetailView(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    template_name = 'post_detail.html'
+    context = {
+        'post': post
+    }
+    return render(request, template_name, context)
